e8d2049db2b1dc8c8f65e7f2b853bff6d5957cc0
nihalshameem <nihalshameem6@gmail.com>
2021-12-05 22:41:36 +0530

    sidebar ui design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,117 +16,120 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.0/css/font-awesome.min.css" integrity="sha512-FEQLazq9ecqLN5T6wWq26hCZf7kPqUbFC9vsHNbXMJtSZZWAcbJspT+/NEAQkBfFReZ8r9QlA9JHaAuo28MTJA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.0/css/font-awesome.min.css"
+        integrity="sha512-FEQLazq9ecqLN5T6wWq26hCZf7kPqUbFC9vsHNbXMJtSZZWAcbJspT+/NEAQkBfFReZ8r9QlA9JHaAuo28MTJA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div id="wrapper">
+        <div id="app">
+            <div class="overlay" style="display:none;" data-toggle="offcanvas"></div>
+            @include('layouts.sidebar')
+            <!-- /#sidebar-wrapper -->
+            {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="">
+                    
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('receipt') }}">Receipts</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('consumption') }}">Consumptions</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('shifting') }}">Shiftings</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('purchase-order') }}">Purchase Order</a>
-                        </li>
-                    </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav me-auto">
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if(Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ms-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        @if(Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link"
+                                    href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
 
-                            @if(Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        @if(Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link"
+                                    href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}"
-                                        method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @php
-                $lastseg = collect(request()->segments())->last();
-            @endphp
-            
-                   <div class="container">
-                   
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-                    <?php $segments = '';?>
-                    @foreach(Request::segments() as $segment)
-                        <?php $segments .= '/' . $segment;?>
-                        <li
-                            class="breadcrumb-item {{ ($lastseg == $segment) ? 'active':'' }}">
-                            <a href="{{ $segments }}">{{ $segment }}</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
                         </li>
-                    @endforeach
-                </ol>
-            </nav>
-                    @if (session('success'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                    @if (session('error'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ session('error') }}
-                        </div>
-                    @endif
+                    @endguest
+                </ul>
+            </div>
+        </div>
+        </nav> --}}
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <button type="button" class="hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
+                <span class="hamb-top"></span>
+                <span class="hamb-middle"></span>
+                <span class="hamb-bottom"></span>
+            </button>
+            <div class="top-nav">
+                <!-- <div class="d-inline">dd</div> -->
+            </div>
+            <div class="container-fluid main">
+                @php
+                    $lastseg = collect(request()->segments())->last();
+                @endphp
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                        <?php $segments = '';?>
+                        @foreach(Request::segments() as $segment)
+                            <?php $segments .= '/' . $segment;?>
+                            <li
+                                class="breadcrumb-item {{ ($lastseg == $segment) ? 'active':'' }}">
+                                <a href="{{ $segments }}">{{ $segment }}</a>
+                            </li>
+                        @endforeach
+                    </ol>
+                </nav>
+                @if(session('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('success') }}
                     </div>
-            @yield('content')
-        </main>
+                @endif
+                @if(session('error'))
+                    <div class="alert alert-danger" role="alert">
+                        {{ session('error') }}
+                    </div>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+    </div>
     </div>
 
     {{-- scripts --}}
@@ -138,56 +141,13 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     @yield('datatables')
-    <script>
-        $('.dropdown-toggle').dropdown()
-        var base = window.location.origin;
-        $('#wh_name').on('change', function () {
-            let id = $('#wh_name').val();
-            $('#lot_number option').remove();
-            $.ajax({
-                type: 'GET',
-                url: base + "/get-lot-numbers/" + id,
-                dataType: "json",
-                success: function (data) {
-                    $.each(data, function (key, value) {
-                        $('#lot_number')
-                            .append($("<option></option>")
-                                .attr("value", value.id)
-                                .text(value.name));
-                    });
-                }
-            });
-        })
-        $('#location_id').on('change', function () {
-            let id = $('#location_id').val();
-            $('#wh_name option').remove();
-            $.ajax({
-                type: 'GET',
-                url: base + "/get-wh-names/" + id,
-                dataType: "json",
-                success: function (data) {
-                    $('#wh_name')
-                        .append($("<option></option>")
-                            .attr("value", '')
-                            .text('--Select--'))
-                    $.each(data, function (key, value) {
-                        $('#wh_name')
-                            .append($("<option></option>")
-                                .attr("value", value.id)
-                                .text(value.name));
-                    });
-                }
-            });
-        })
-
-    </script>
 </body>
 
 </html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,30 @@
+<!-- Sidebar -->
+<nav class="navbar navbar-inverse fixed-top" id="sidebar-wrapper" role="navigation">
+    <ul class="nav sidebar-nav">
+        <div class="sidebar-header">
+            <div class="sidebar-brand">
+                <a
+                    href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+            </div>
+        </div>
+        <li><a class="home" href="{{ url('/home') }}">Dashboard</a></li>
+        <li><a class="receipt" href="{{ url('receipt') }}">Receipts</a></li>
+        <li><a class="consumption" href="{{ url('consumption') }}">Consumptions</a></li>
+        <li><a class="shifting" href="{{ url('shifting') }}">Shiftings</a></li>
+        <li><a class="purchase-order" href="{{ url('purchase-order') }}">Purchase
+                Order</a>
+        </li>
+        {{-- dropdown --}}
+        {{-- <li class="dropdown">
+            <a href="#works" class="dropdown-toggle" data-toggle="dropdown">Works <span class="caret"></span></a>
+            <ul class="dropdown-menu animated fadeInLeft" role="menu">
+                <div class="dropdown-header">Dropdown heading</div>
+                <li><a href="#" class="pictures">Pictures</a></li>
+                <li><a href="#" class="videos">Videeos</a></li>
+                <li><a href="#" class="books">Books</a></li>
+                <li><a href="#" class="art">Art</a></li>
+                <li><a href="#" class="awards">Awards</a></li>
+            </ul>
+        </li> --}}
+    </ul>
+</nav>
